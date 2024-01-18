17a6d072d1c626138f3e724e0d7f7169e24a2992
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2024-01-18 15:09:05 +0100

    feat: started training function

---
+++ QES-GAN/configs/training_config.py
@@ -0,0 +1,13 @@
+
+
+N_EPOCHS = 2
+
+#####################
+## IMAGE VARIABLES ##
+#####################
+
+IMAGE_SIZE = 14  # TODO: small for developing purposes. Could increase to 28
+CLASSES = [0,1]  # This only works for MNIST, picks number classes as specified in list
+
+
+
