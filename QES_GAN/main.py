e23ee5054b66d4ad3ba212b6a157f9e6c7105af7
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2024-02-15 15:55:00 +0100

    style: added main() loop for tidiness

--- QES_GAN/main.py
+++ QES_GAN/main.py
@@ -1,59 +1,64 @@
 import torch
 import os
 from networks.critic import ClassicalCritic
-import QES_GAN as qes
+import QES_GAN as qes_gan
 import configs.training_config as training_config
 
 
-n_data_qubits = training_config.N_DATA_QUBITS
-n_ancilla = training_config.N_ANCILLAS
-image_shape = (training_config.IMAGE_SIDE, training_config.IMAGE_SIDE)
-batch_size = training_config.BATCH_SIZE
-n_children = training_config.N_CHILDREN
-n_max_evaluations = training_config.M_MAX_EVALUATIONS
-shots = training_config.SHOTS
-simulator = training_config.SIMULATOR
-noise = training_config.NOISE
-dtheta = training_config.DTHETA
-action_weights = training_config.ACTION_WEIGHTS
-multi_action_pb = training_config.MULTI_ACTION_PB
-max_gen_no_improvements = training_config.MAX_GEN_NO_IMPROVEMENT
-max_depth = training_config.MAX_DEPTH
+def main():
+    n_data_qubits = training_config.N_DATA_QUBITS
+    n_ancilla = training_config.N_ANCILLAS
+    image_shape = (training_config.IMAGE_SIDE, training_config.IMAGE_SIDE)
+    batch_size = training_config.BATCH_SIZE
+    n_children = training_config.N_CHILDREN
+    n_max_evaluations = training_config.M_MAX_EVALUATIONS
+    shots = training_config.SHOTS
+    simulator = training_config.SIMULATOR
+    noise = training_config.NOISE
+    dtheta = training_config.DTHETA
+    action_weights = training_config.ACTION_WEIGHTS
+    multi_action_pb = training_config.MULTI_ACTION_PB
+    max_gen_no_improvements = training_config.MAX_GEN_NO_IMPROVEMENT
+    max_depth = training_config.MAX_DEPTH
 
 
-# device = torch.device("cpu")
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-if torch.cuda.is_available():
-    gpu = True
-    print('GPU available')
-else:
-    gpu = False
+    # device = torch.device("cpu")
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.is_available():
+        gpu = True
+        print('GPU available')
+    else:
+        gpu = False
 
-print(f'Using device: {device}')
-critic_net = ClassicalCritic(image_shape=(training_config.IMAGE_SIDE, training_config.IMAGE_SIDE))
-critic_net = critic_net.to(device)
-current_working_directory = os.getcwd()
-critic_net_path = current_working_directory + '/output/' + "critic-510.pt"
-print(f'critic net path: {critic_net_path}')
-# Import pre-trained critic net
-critic_net.load_state_dict(torch.load(critic_net_path))
+    print(f'Using device: {device}')
+    critic_net = ClassicalCritic(image_shape=(training_config.IMAGE_SIDE, training_config.IMAGE_SIDE))
+    critic_net = critic_net.to(device)
+    current_working_directory = os.getcwd()
+    critic_net_path = current_working_directory + '/output/' + "critic-510.pt"
+    print(f'critic net path: {critic_net_path}')
+    # Import pre-trained critic net
+    critic_net.load_state_dict(torch.load(critic_net_path))
 
-qes = qes.Qes(n_data_qubits=n_data_qubits,
-              n_ancilla=n_ancilla,
-              image_shape=image_shape,
-              batch_size=batch_size,
-              critic_net=critic_net,
-              n_children=n_children,
-              n_max_evaluations=n_max_evaluations,
-              shots=shots,
-              simulator=simulator,
-              noise=noise,
-              gpu=gpu,
-              device=device,
-              dtheta=dtheta,
-              action_weights=action_weights,
-              multi_action_pb=multi_action_pb,
-              max_gen_no_improvement=max_gen_no_improvements,
-              max_depth=max_depth)
+    qes = qes_gan.Qes(n_data_qubits=n_data_qubits,
+                  n_ancilla=n_ancilla,
+                  image_shape=image_shape,
+                  batch_size=batch_size,
+                  critic_net=critic_net,
+                  n_children=n_children,
+                  n_max_evaluations=n_max_evaluations,
+                  shots=shots,
+                  simulator=simulator,
+                  noise=noise,
+                  gpu=gpu,
+                  device=device,
+                  dtheta=dtheta,
+                  action_weights=action_weights,
+                  multi_action_pb=multi_action_pb,
+                  max_gen_no_improvement=max_gen_no_improvements,
+                  max_depth=max_depth)
 
-qes.data()
+    qes.data()
+
+
+if __name__ == '__main__':
+    main()
